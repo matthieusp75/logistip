7732e7674d158ad0c23785fef87ca9eed47c7bb3
jean francois barbato <jeanf.barbato@gmail.com>
2022-09-17 09:42:18 +0200

    update order line + validate order

--- app/policies/order_line_policy.rb
+++ app/policies/order_line_policy.rb
@@ -1,13 +1,9 @@
-class OrderPolicy < ApplicationPolicy
+class OrderLinePolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    def resolve
-      scope.all
-    end
-  end
-
-  def show?
-    true
+    # def resolve
+    #   scope.all
+    # end
   end
 
   def create?
@@ -15,10 +11,6 @@ class OrderPolicy < ApplicationPolicy
   end
 
   def update?
-    true
-  end
-
-  def validate?
     true
   end
 end
